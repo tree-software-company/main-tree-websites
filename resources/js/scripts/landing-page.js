3d91f18d03a2dbbfe0bac3c976eaa42fdf53270b
simon04PL <szymon.msa@gmail.com>
2024-12-28 12:13:06 +0100

    repair console error

--- resources/js/scripts/landing-page.js
+++ resources/js/scripts/landing-page.js
@@ -1,35 +1,32 @@
-// resources/js/landing.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const otaButton = document.querySelector('.btn-ota');
     const registrationForm = document.querySelector('.registration-form');
     const closeIcon = document.querySelector('.close-form');
   
-    // Show the registration form on button click
-    otaButton.addEventListener('click', function () {
-      registrationForm.style.display = 'block';
-    });
+    if (otaButton) {
+        otaButton.addEventListener('click', function () {
+            registrationForm.style.display = 'block';
+        });
+    }
   
-    // Hide the registration form on close icon click
-    closeIcon.addEventListener('click', function () {
-      registrationForm.style.display = 'none';
-    });
+    if (closeIcon) {
+        closeIcon.addEventListener('click', function () {
+            registrationForm.style.display = 'none';
+        });
+    }
   
-    // Accordion functionality
     const accordionQuestions = document.querySelectorAll('.accordion-question');
   
     accordionQuestions.forEach(question => {
-      question.addEventListener('click', function () {
-        const item = question.parentNode;
-        item.classList.toggle('active');
+        question.addEventListener('click', function () {
+            const item = question.parentNode;
+            item.classList.toggle('active');
   
-        // Close other accordion items
-        accordionQuestions.forEach(q => {
-          if (q !== question) {
-            q.parentNode.classList.remove('active');
-          }
+            accordionQuestions.forEach(q => {
+                if (q !== question) {
+                    q.parentNode.classList.remove('active');
+                }
+            });
         });
-      });
     });
-  });
-  
+});
